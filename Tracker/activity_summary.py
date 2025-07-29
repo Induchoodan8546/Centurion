9852d374134cd483d0b755838993d63177f873b3
INDUCHOODAN V S <induchoodanvs2004@gmail.com>
2025-07-29 18:17:51 +0530

    graph plotting starteed

--- Tracker/activity_summary.py
+++ Tracker/activity_summary.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from datetime import datetime
 def print_activity_summary():
     df = pd.read_csv("activity_log.csv")
@@ -57,7 +58,52 @@ def print_app_usage_summary():
     for app, duration in sorted_apps:
         print(f"{app} | {duration}")
 
+def plot_app_usage(file_path="activity_log.csv"):
+    df = pd.read_csv(file_path)
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df = df.dropna(subset=['application'])
+    app_usage = {}
+
+    app_usage = {}
+    for i in range(len(df)-1):
+        current = df.iloc[i]
+        next = df.iloc[i + 1]
+        duration = next['timestamp'] - current['timestamp']
+        
+        raw_app = current['application']
+        if not isinstance(raw_app, str):
+            continue
+        if "Visual Studio Code" in raw_app:
+            app = "VS Code"
+        elif "Google Chrome" in raw_app:
+            app = "Chrome"
+        elif "Brave" in raw_app:
+            app = "Brave"
+        else:
+            app = raw_app
+
+        app_usage[app] = app_usage.get(app, pd.Timedelta(0)) + duration
+        app_names = []
+        app_durations = []
+        for app, duration in app_usage.items():
+            minutes = duration.total_seconds() / 60
+            app_names.append(app)
+            app_durations.append(minutes)
+        plt.figure(figsize=(10, 5))
+        bars = plt.bar(app_names, app_durations, color='skyblue')
+        plt.title('Application Usage Over Time')
+        plt.xlabel('Applications')
+        plt.ylabel('Usage Duration (minutes)')
+        plt.xticks(rotation=45, ha='right')
+        for bar, value in zip(bars, app_durations):
+            plt.text(bar.get_x() + bar.get_width() / 2, bar.get_height() + 1, f'{value:.1f}', ha='center', va='bottom')
+            plt.tight_layout()
+            plt.show()
+
+
+    
 if __name__ == "__main__":
 
     print_activity_summary()
     print_app_usage_summary()
+    plot_app_usage()
